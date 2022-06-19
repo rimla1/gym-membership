548f5bfbf3dd41dee2d546b77f0729dde51842b2
Almir Muminovic <projectalmir@gmail.com>
2022-06-19 19:45:46 +0200

    established db connection

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
+import {config} from 'dotenv'
+config()
+
 import express from 'express';
+import mongoose from 'mongoose';
 import { userRouter } from './module/User/user.routes';
 
 const PORT = 3000;
+
+const MONGODB_URI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.ekxmb.mongodb.net/${process.env.MONGODB_DATABASE_NAME}?retryWrites=true&w=majority`;
 
 const app = express();
 
@@ -9,7 +15,10 @@ app.use(express.json())
 
 app.use("/api/v1/users", userRouter)
 
-
-app.listen(PORT, () => {
-    console.log(`Up and running on port ${PORT}`)
+mongoose.connect(MONGODB_URI).then(() => {
+    console.log("Connected to the db")
+    app.listen(PORT, () => {
+        console.log(`Up and running on port ${PORT}`)
+    })
 })
+
