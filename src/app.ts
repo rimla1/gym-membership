a82685035cde12ec251a930613ee1714daf5a035
Almir Muminovic <projectalmir@gmail.com>
2022-06-19 18:59:36 +0200

    infra setup

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.json())
+
+app.listen(PORT, () => {
+    console.log(`Up and running on port ${PORT}`)
+})
