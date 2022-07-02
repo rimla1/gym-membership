4a060cc6082500cfcb44ee0b6f224dd3d3600489
Almir Muminovic <projectalmir@gmail.com>
2022-07-02 18:59:38 +0200

    Connecting auth service with user service

--- src/module/Auth/auth.controller.ts
+++ src/module/Auth/auth.controller.ts
@@ -1,8 +1,12 @@
 import {Request, Response} from "express";
+import { UserRepository } from "../User/user.repository";
+import { UserService } from "../User/user.service";
 import { AuthService } from "./auth.service";
 import { LoginRequest } from "./auth.types";
 
-const authService = new AuthService
+const userRepostiory = new UserRepository()
+const userService = new UserService(userRepostiory)
+const authService = new AuthService(userService)
 
 export const loginUser = (req: Request, res: Response) => {
     const {email, password} = req.body
--- src/module/Auth/auth.service.ts
+++ src/module/Auth/auth.service.ts
@@ -1,12 +1,21 @@
 import { ErrorNotFound } from "../../shared/types";
+import { UserService } from "../User/user.service";
 import { LoginRequest, LoginResponse } from "./auth.types";
 
 interface IAuthService{
-    
+    login(loginInput: LoginRequest): Promise<LoginResponse | ErrorNotFound>
 }
 
 export class AuthService implements IAuthService{
+
+    userService: UserService
+
+    constructor(userService: UserService){
+        this.userService = userService
+    }
+
     async login(loginInput: LoginRequest): Promise<LoginResponse | ErrorNotFound>{
+        const user = await this.userService.getUserByEmail(loginInput.email)
         return {
             message: "User not found"
         }
