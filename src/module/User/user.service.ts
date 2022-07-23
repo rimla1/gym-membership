255eac108fee2b184a6e37f11f33839137e7ea20
Almir Muminovic <projectalmir@gmail.com>
2022-07-23 17:48:27 +0200

    Edit user password field problem solving

--- src/module/User/user.service.ts
+++ src/module/User/user.service.ts
@@ -1,6 +1,7 @@
 import { UserRepository } from "./user.repository"
 import { CreateUserInput, EditUserInput, User } from "./user.types"
 import bcrypt from "bcrypt";
+import { AlreadyExistsError } from "../../shared/errors";
 
 interface IUserService {
     createUser(createUserInput: CreateUserInput): Promise<User>
@@ -74,12 +75,21 @@ export class UserService implements IUserService {
 
     async editUser(userId: string, editUserInput: EditUserInput): Promise<User> {
         try {
-            const editedUser = await this.userRepo.editUser(userId, editUserInput)
+            
+            const hashedPassword = await this.hashPassword(editUserInput.password)
+            const editedUser = await this.userRepo.editUser(userId, {...editUserInput, password: hashedPassword})
+            console.log(hashedPassword)
+            console.log(editUserInput.password)
+            const match = await bcrypt.compare(editUserInput.password, editedUser.password)
+            if(match){
+                throw new AlreadyExistsError("Password already exist! Please try different password!")
+            }
             return editedUser  
         } catch (error) {
             throw error
         }
-
+        // $2b$10$DHq4H7OSTG1U00xB.wpOfe9Y/fSxydx9Iifth07JqozE5ybh..xXq // Current password [random]
+        // $2b$10$cmpiQceepFTGlVrVaJOjw.5G4t4ICW/B5vSXPjaz1gimDqaNOVg9i //New different password from previous [almir1234]
     }
 
     private async hashPassword(passwordToHash: string){
